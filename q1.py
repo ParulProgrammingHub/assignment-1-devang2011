5a6e1d8eee668074c863e9d6bdbb52b9844c6434
devang2011 <dev.gadesha1996@gmail.com>
2017-02-14 13:34:18 +0530

    Create q1.py

---
+++ q1.py
@@ -0,0 +1,4 @@
+l=float(input(" Enter the length "))
+b=float(input( " Enter the breadth "))
+print (" The area of the rectangle is ", (l*b)) 
+print (" The parameter of rectangle is ", (2*l) + (2*b))
