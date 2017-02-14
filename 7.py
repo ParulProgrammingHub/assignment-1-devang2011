835a64bea4eabdc090abfa8851faf00c84473019
devang2011 <dev.gadesha1996@gmail.com>
2017-02-14 13:43:07 +0530

    Create 7.py

---
+++ 7.py
@@ -0,0 +1,6 @@
+angle1=float(input( " Enter the first angle "))
+angle2=float(input( " Enter the second angle "))
+defthirdangle(angle1,angle2) :
+         angle3=180-(angle1+angle2)
+print ( " Third angle of triangle is ", angle3)
+thirdangle(angle1 , angle2 )
