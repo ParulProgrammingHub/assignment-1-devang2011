e1554b0e4b2d3a08406256787a298e6a6d607f7f
devang2011 <dev.gadesha1996@gmail.com>
2017-02-14 13:47:07 +0530

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,7 @@
+pri = float(input(" Enter the principal "))
+rate = float(input(" Enter the interest rate "))
+years= float(input(" Enter the number of years "))
+def simple_interest(pri,rate,years):
+       simple_interest=float((pri*rate*years/100))
+       returnsimple_interest
+print(" Simple interest is ", simple_interest(pri,rate,years))
